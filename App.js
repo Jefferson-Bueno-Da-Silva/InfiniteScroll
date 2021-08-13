27bf4899ae04c1bef530699997766609dc86689e
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-08-13 09:42:45 -0300

    infinit scroll

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'
+
+import axios from 'axios'
 
 export default function App() {
+  const baseURL = 'https://api.github.com'
+  const perPage = 20
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    loadApi()
+  }, [])
+
+  const loadApi = async () => {
+    if (loading) return
+
+    setLoading(true)
+    // /search/repositories?q=react&per_page=5&page=1
+    const response = await axios.get(
+      `${baseURL}/search/repositories?q=react&per_page=${perPage}&page=${page}`,
+    )
+
+    setData([...data, ...response.data.items])
+    setPage(page + 1)
+    setLoading(false)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <FlatList
+        data={data}
+        style={{ marginTop: 30 }}
+        contentContainerStyle={{ marginHorizontal: 20 }}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <ListItem data={item} />}
+        onEndReached={loadApi}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={<FooterList load={loading} />}
+      />
     </View>
-  );
+  )
+}
+const ListItem = ({ data }) => {
+  const { id, full_name } = data
+  return (
+    <View style={styles.listItem}>
+      <Text style={styles.listText}>{full_name}</Text>
+    </View>
+  )
+}
+const FooterList = ({ load }) => {
+  if(!load) return null;
+
+  return (
+    <View style={styles.loading}>
+      <ActivityIndicator size={25} color="#121212" />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  listItem: {
+    backgroundColor: '#121212',
+    padding: 30,
+    marginTop: 20,
+    borderRadius: 10,
+  },
+  listText: {
+    fontSize: 16,
+    color: '#fff',
+  },
+  loading: {
+    padding: 10,
+  },
+})
